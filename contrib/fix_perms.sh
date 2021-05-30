3ad4c5b8218992e59e168e50f09ecdcbc7592b09
Teagan glenn <teagan.m.glenn@gmail.com>
2021-05-30 17:48:56 -0600

    Set all to root after build

--- contrib/fix_perms.sh
+++ contrib/fix_perms.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo chown root:root ./_build/dev/lib/blue_heron_transport_usb/priv/hci_transport
-sudo chmod +s ./_build/dev/lib/blue_heron_transport_usb/priv/hci_transport
+sudo chown -R root:root ./_build/dev/
+sudo chmod -R +s ./_build/dev/
